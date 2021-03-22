d16c944a3b183f6f25af297af6b9a81db6a5cf3f
Romain Desarzens <hib.pub@protonmail.com>
2021-03-22 22:11:23 +0100

    Start python implementation

--- python/src/track.py
+++ python/src/track.py
@@ -1,2 +1,76 @@
+import typing
+import numpy as np
 
 
+class ImageFrame(typing.NamedTuple):
+    image: np.ndarray
+    width: int
+    height: int
+
+
+class TrackState(typing.NamedTuple):
+    x: float
+    y: float
+    x_dot: float
+    y_dot: float
+    width: int
+    height: int
+
+    def position(self):
+        return np.array([self.x, self.y])
+
+
+class Track(typing.NamedTuple):
+    state: TrackState
+    frame: ImageFrame
+
+
+class MeasurementState(typing.NamedTuple):
+    x: float
+    y: float
+    width: int
+    height: int
+
+    def position(self):
+        return np.array([self.x, self.y])
+
+
+class Measurement(typing.NamedTuple):
+    state: MeasurementState
+    frame: ImageFrame
+
+
+def compute_spatio_temporal_cost(track: Track, measurement: Measurement) -> float:
+    """
+    Compute spatio-temporal cost between a track and a new measurement as a normalized Euclidian distance (normalized
+    by width and height of the image frame)
+    :param track:
+    :param measurement:
+    :return: cost
+    """
+    return np.linalg.norm((track.state.position() - measurement.state.position()) * np.array(
+        [1. / track.frame.width, 1. / track.frame.height]))
+
+
+def compute_total_cost(track: Track, measurement: Measurement) -> float:
+    """
+    Compute total association cost between a track and a new measurement (spatio-temporal + visual cost)
+    :param track:
+    :param measurement:
+    :return:
+    """
+    return compute_spatio_temporal_cost(track, measurement)
+
+
+def compute_cost_matrix(tracks: typing.List[Track], measurements: typing.List[Measurement]) -> np.ndarray:
+    """
+    For each pair of track and measurement, compute the associated cost and return the associated matrix
+    :param tracks: tracks
+    :param measurements: measurement
+    """
+    cost_matrix = np.empty((len(tracks), len(measurements)))
+    for track_index, track in enumerate(tracks):
+        for measurement_index, measurement in enumerate(measurements):
+            cost_matrix[track_index][measurement_index] = compute_total_cost(track, measurement)
+
+    return cost_matrix
--- python/tests/tracks_tests.py
+++ python/tests/tracks_tests.py
@@ -1,8 +1,36 @@
 import unittest
+from scipy.optimize import linear_sum_assignment
+import numpy as np
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from python.src.track import compute_cost_matrix, Track, Measurement, ImageFrame, MeasurementState, TrackState, \
+    compute_spatio_temporal_cost
 
-if __name__ == '__main__':
-    unittest.main()
+
+def make_empty_frame(width: int, height: int) -> ImageFrame:
+    return ImageFrame(image=np.array([]), width=width, height=height)
+
+
+def make_no_speed_track_state(x: float, y: float, width: int, height: int) -> TrackState:
+    return TrackState(x=x, y=y, width=width, height=height, x_dot=0., y_dot=0.)
+
+
+class TracksTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.mock_measurement = Measurement(state=MeasurementState(x=4., y=6., width=10, height=10),
+                                            frame=make_empty_frame(100, 100))
+
+        self.mock_track = Track(state=make_no_speed_track_state(x=4., y=6., width=10, height=10),
+                                frame=make_empty_frame(100, 100))
+
+    def test_compute_spatio_temporal_cost_same_measurement_and_track(self):
+        self.assertAlmostEqual(compute_spatio_temporal_cost(self.mock_track, self.mock_measurement), 0.)
+
+    def test_linear_sum_assignment(self):
+        cost_matrix = np.array([[1., 2., 3.],
+                                [2., 2., 3.],
+                                [1., 4., 6.]])
+
+        row_ind, col_ind = linear_sum_assignment(cost_matrix, maximize=True)
+
+        if __name__ == '__main__':
+            unittest.main()
